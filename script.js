05ed6bc73d0d2d6fd5c293c21239203c6beba4d5
RannEscobar <168644141+RannEscobar@users.noreply.github.com>
2024-06-21 13:47:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    const message = "Happy Birthday Nisaaa! Semoga di usia yang ke-19 tahun ini, semua impian dan harapanmu dapat tercapai. Semoga selalu sehat, bahagia, dan sukses dalam segala hal. Dan jangan lupa untuk selalu tetap bernapas.";
+    document.getElementById("message").textContent = message;
+}
